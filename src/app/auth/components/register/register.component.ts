1ed6794e8fd84dbba9dbd09b1b5dcd308a5adab9
maciek <maciek29.opozda@gmail.com>
2025-02-05 09:27:57 +0100

    REG-2 Add RegisterPage Layout, and basic handle on register form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,11 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  formBuilder = inject(FormBuilder);
+
+  registerForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit(){
+    console.log("123", this.registerForm.getRawValue())
+  }
+}
+
+//@TODO Different place form form declaration, on the component should be only instatniate, or call//
